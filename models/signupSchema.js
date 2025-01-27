87d4dbc903ebdb7cf3e9ee675f3b0cf53df4ab81
boopathi2706 <boopathi.v2023cse@sece.ac.in>
2025-01-27 16:08:00 +0530

    day 13 afternoon commit

--- models/signupSchema.js
+++ models/signupSchema.js
@@ -1,10 +1,10 @@
-const mdb=require("mongoose");
-const signupSchema=mdb.Schema({
-    firstname:String,
-    lastname:String,  
-    email:String,
-    password:String,
+const mdb = require("mongoose");
+const signupSchema = mdb.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
 });
 
-const signup_schema=mdb.model("signup",signupSchema);
-exports.module=signup_schema;
+const Signup_schema = mdb.model("signup", signupSchema);
+module.exports = Signup_schema;
